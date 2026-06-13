2646a36875799886d8e8770d14402e90267c5efa
Audran <audran.bert@alumni.univ-avignon.fr>
2026-06-13 16:04:13 +0200

    Updates

--- assets/ManagementScreen-BFMNDFwo.js
+++ assets/ManagementScreen-BFMNDFwo.js
@@ -1,4 +1,4 @@
-import{c as k,r,j as e,t as n,a as f,T,d as O}from"./index-DMY_QTVK.js";/**
+import{c as k,r,j as e,t as n,a as f,T,d as O}from"./index-BiVCAxKq.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
