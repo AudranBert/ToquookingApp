326d93ae10f7e4ea69ed5347652637c2400c282b
Audran <audran.bert@alumni.univ-avignon.fr>
2026-06-13 15:42:22 +0200

    Updates

--- assets/ManagementScreen-B5se4F2W.js
+++ assets/ManagementScreen-B5se4F2W.js
@@ -1,4 +1,4 @@
-import{c as k,r,j as e,t,a as f,T,D as O}from"./index-BkOIFkmK.js";/**
+import{c as k,r,j as e,t,a as f,T,D as O}from"./index-DnrVYtdx.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
