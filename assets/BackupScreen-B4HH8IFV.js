3826a1229efed9f42c1a09c91d0c4a8b883a2f21
Audran <audran.bert@alumni.univ-avignon.fr>
2026-06-13 15:22:05 +0200

    Updates

--- assets/BackupScreen-B4HH8IFV.js
+++ assets/BackupScreen-B4HH8IFV.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,M as r,s as h,S as d,t as a,U as b}from"./index-D-8xwhhW.js";/**
+import{c as t,j as e,M as r,s as h,S as d,t as a,U as b}from"./index-D1wAvz-5.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
