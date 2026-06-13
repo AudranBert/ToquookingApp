d6fdf2be8adbeef632b9d19c0225132005d5f769
Audran <audran.bert@alumni.univ-avignon.fr>
2026-06-13 21:41:16 +0200

    Updates

--- assets/BackupScreen-COckLwP4.js
+++ assets/BackupScreen-COckLwP4.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,M as r,s as h,S as d,t as a,U as b}from"./index-BiCJqMXE.js";/**
+import{c as t,j as e,M as r,s as h,S as d,t as a,U as b}from"./index-BoSfIr-d.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
